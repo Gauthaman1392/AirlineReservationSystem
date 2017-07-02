e0a2665c19016eac0bb90b1b3a69d7f3ac44daa7
Gauthaman1392 <gauthammukesh92@gmail.com>
2017-07-01 20:50:30 -0500

    Developer: Gauthaman Deenadayalan

---
+++ AirlineReservation-master/AirlineRESTServices/src/main/java/trng/imcs/restwebservices/controller/RestServiceController.java
@@ -0,0 +1,60 @@
+package trng.imcs.restwebservices.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import trng.imcs.restwebservices.beans.FlightInformation;
+import trng.imcs.restwebservices.services.FlightService;
+
+@RestController
+@RequestMapping("/rest")
+public class RestServiceController {
+
+	@Autowired
+	FlightService flightService;
+
+	@RequestMapping(value = "/{flightID}", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_HTML_VALUE })
+	public ResponseEntity<?> getFlight(@PathVariable String flightID) {
+
+		FlightInformation flightInfo = flightService.getFlightInfo(Integer.parseInt(flightID));
+		if (flightInfo == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(flightInfo, HttpStatus.ACCEPTED);
+		}
+	}
+
+	@RequestMapping(method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_HTML_VALUE })
+	public ResponseEntity<?> createFlight(@RequestBody FlightInformation flightInfo) {
+
+		FlightInformation newFlightInfo = flightService.saveFlightDetails(flightInfo);
+		return new ResponseEntity<>(newFlightInfo, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	public FlightInformation updateFlight(@RequestBody FlightInformation flightInfo){
+		return flightService.updateFlight(flightInfo);
+	}
+
+	
+	 @RequestMapping(value = "/{flightID}", method = RequestMethod.DELETE) public
+	  ResponseEntity<?> deleteFlight(@PathVariable Integer flightID) { 
+		 Integer fId = flightService.deleteFlight(flightID); 
+		 if (fId == null) {
+	  return new ResponseEntity<>(HttpStatus.BAD_REQUEST); } else {
+	  
+	  return new ResponseEntity<>(HttpStatus.ACCEPTED); }
+	  
+	  }
+}
---
+++ AirlineReservation-master/AirlineSOAPServices/src/main/java/org/AirlineSOAPServices/App.java
@@ -0,0 +1,13 @@
+package org.AirlineSOAPServices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
